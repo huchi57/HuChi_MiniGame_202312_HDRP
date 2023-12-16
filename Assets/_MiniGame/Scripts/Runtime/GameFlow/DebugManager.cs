11626b06182c81a86e8447b3486cb6559531bad4
huchi57 <huchi.hello@gmail.com>
2023-12-16 13:51:45 +0900

    Fix: Made Debug Display a GameObject

--- Assets/_MiniGame/Scripts/Runtime/GameFlow/DebugManager.cs
+++ Assets/_MiniGame/Scripts/Runtime/GameFlow/DebugManager.cs
@@ -7,22 +7,13 @@ namespace UrbanFox.MiniGame
 {
     public class DebugManager : RuntimeManager<DebugManager>
     {
-        private static readonly Rect k_debugRect = new Rect(100, 100, 1000, 1000);
-
         [Header("Components")]
         [SerializeField, Required] private CinemachineVirtualCamera m_cinemachineVirtualCamera;
+        [SerializeField, Required] private Text m_debugText;
         [SerializeField] private GameObject m_debugGraph;
 
         [Header("Display Options")]
         [SerializeField] private bool m_enableDebug;
-
-        [Space]
-
-        [SerializeField] private int m_warningFPS = 60;
-        [SerializeField] private Color m_warningFPSColor = Color.red;
-
-        [SerializeField] private int m_criticalFPS = 30;
-        [SerializeField] private Color m_criticalFPSColor = Color.red;
 
         private void Start()
         {
@@ -42,13 +33,9 @@ namespace UrbanFox.MiniGame
                     m_debugGraph.SetActive(m_enableDebug);
                 }
             }
-        }
-
-        private void OnGUI()
-        {
-            if (m_enableDebug && Application.isPlaying)
+            if (m_debugGraph.activeSelf)
             {
-                GUI.Label(k_debugRect, GetDebugContent());
+                m_debugText.text = GetDebugContent();
             }
         }
 
@@ -57,13 +44,10 @@ namespace UrbanFox.MiniGame
             var content = new StringBuilder();
             if (GameManager.IsInstanceExist && ApplicationBuildData.Instance)
             {
-                content.AppendLine($"Press F6 to toggle display ON/OFF.");
                 content.AppendLine($"{ApplicationBuildData.Instance.GetBuildInfoText()}");
                 content.AppendLine($"Game State: {GameManager.Instance.CurrentGameState}");
                 content.AppendLine($"FOV: {m_cinemachineVirtualCamera.m_Lens.FieldOfView:F2}");
 
-                var fps = 1 / Time.unscaledDeltaTime;
-                content.AppendLine($"FPS: {(int)fps} ({Time.unscaledDeltaTime:F2}ms)".Color(fps < m_criticalFPS ? m_criticalFPSColor : fps < m_warningFPS ? m_warningFPSColor : Color.white));
                 // TODO: Add more texts
             }
             return content.ToString();
